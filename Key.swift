2fcec66a21b5ffd699e471743afe08aee978d9be
Jackson <jacksongerambia@yahoo.com>
2025-12-09 07:49:27 -0600

    commit 1 (Jackson)

--- Key.swift
+++ Key.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
+//  Key.swift
 //  Limbo Keys Game
 //
 //  Created by JACKSON GERAMBIA on 12/9/25.
 //
 
 import Foundation
+import SwiftUI
+
+class Key {
+    var image = "key"
+    var x : Int
+    var y : Int
+    // number of key: 0-7
+    var id : Int
+    
+    init(image: String = "key", x: Int, y: Int, id: Int) {
+        self.image = image
+        self.x = x
+        self.y = y
+        self.id = id
+    }
+}
