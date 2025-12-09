6e71b3d55151076ab882dab8e534a0da42aa04be
kinkyMingus <david.show5c@gmail.com>
2025-12-09 07:56:35 -0600

    LazyVGrid

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var key: Key
+    @State var key = Key(image: "key",x: 0,y: 0,id: 0)
     var body: some View {
         VStack {
             LazyVGrid(
