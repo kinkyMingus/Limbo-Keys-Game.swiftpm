10c9b7cfa3a199d43cf8b8caa6d3aa2008dc729b
kinkyMingus <david.show5c@gmail.com>
2025-12-09 07:54:36 -0600

    LazyVGrid

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var key: Key
     var body: some View {
         VStack {
-           
+            LazyVGrid(
+                columns: [GridItem(.flexible()), GridItem(.flexible())],
+                spacing: 8
+            ) {
+
+                ForEach(0..<8) { i in
+                    Image(key.image)
+                }
+            }
         }
     }
 }
