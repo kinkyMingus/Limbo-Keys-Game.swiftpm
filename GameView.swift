2273f7288101ff37116c2ed5972808f23b2dc2b5
Jackson <jacksongerambia@yahoo.com>
2025-12-10 08:08:15 -0600

    commit 12/10

--- GameView.swift
+++ GameView.swift
@@ -8,26 +8,40 @@
 import SwiftUI
 
 struct GameView: View {
-    @State var keys: [Key] = []
+    @State var keys: [[Key]] = []
+    @State var move: Bool = false
+    
+    var columns : [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    
     var body: some View {
         VStack {
             LazyVGrid(
-                columns: [GridItem(.flexible()), GridItem(.flexible())],
+                columns: columns,
                 spacing: 8
             ) {
 
-                ForEach(keys, id: \.id) { key in
-                    Button {
-                        
-                    } label: {
-                        Image(key.image)
+                ForEach(keys, 0..<keys.count) { r in
+                    ForEach(keys[r], 0..<keys[r].count){ c in
+                        Button {
+                            withAnimation(.easeInOut(diration: 0.5)) {
+                                move.toggle()
+                            }
+                        } label: {
+                            Image(keys[r][c].image)
+                                .resizable()
+                                .scaledToFit()
+                        }
                     }
+                    
                 }
             }
         }
         .onAppear() {
-            for i in 0..<8 {
-                keys.append(Key(x: 0, y: 0, id: i))
+            for i in 0..<4 {
+                for _ in 0..<2 {
+                    keys[i].append(Key(x: 0, y: 0, id: i))
+                }
             }
         }
     }
